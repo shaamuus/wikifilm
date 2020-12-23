117c3444c3950182c96b36bf812a9e74974f621c
Shamus Birch <th35h4y@gmail.com>
2020-12-23 22:53:23 +0000

    add film form inputs

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
  $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
+    $('.datepicker').datepicker({
+        format: "yyyy",
+        minDate: 01-01-1900,
+        maxDate: 01-01-2050,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
